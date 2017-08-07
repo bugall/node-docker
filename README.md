aa8ff9ddada399f7afb774fbca1856218f0c90a2
bugall <bugall@sina.cn>
2017-08-07 15:41:13 +0800

    finsh create stop start backup

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ### 软件
-1. 需要预先安装docker-compose
-
-注意： 初次需要下载镜像, 需要花费较长时间
+1. 需要安装docker
+2. 本机需要启动mongodb
 
 ### 启动
-node index.js
+npm start
--- routes/project/index.js
+++ routes/project/index.js
@@ -4,13 +4,8 @@ const Docker = require('../../lib');
 const docker = new Docker();
 
 router.get('/start/:projectId', function (req, res) {
-	const projectId = req.params['projectId'];
-	// get proejctId config info
-});
-
-router.get('/stop/:projectId', function (req, res) {
     const projectId = req.params['projectId'];
-    docker.stopContainer({ projectId }).then(() => {
+    docker.startContainer({ projectId: projectId }).then(() => {
         res.send({
             ack: 'SUCCESS',
             msg: ''
@@ -19,7 +14,23 @@ router.get('/stop/:projectId', function (req, res) {
         console.log(err.stack);
         res.send({
             ack: 'error',
+            msg: err.message
+        });
+    });
+});
+
+router.get('/stop/:projectId', function (req, res) {
+    const projectId = req.params['projectId'];
+    docker.stopContainer({ projectId: projectId }).then(() => {
+        res.send({
+            ack: 'SUCCESS',
             msg: ''
+        });
+    }).catch((err) => {
+        console.log(err.stack);
+        res.send({
+            ack: 'error',
+            msg: err.message
         });
     });
 });
@@ -29,23 +40,72 @@ router.post('/:projectId', function(req,res, next) {
 	const image = req.body['image'];
 	const entrypoint = req.body['entrypoint'];
     
-    docker.createContainer({ projectId, image, entrypoint }).then(() => {
+    docker.createContainer({ 
+        projectId: projectId, 
+        image: image,
+        entrypoint:entrypoint 
+    }).then(() => {
         res.send({
             ack: 'SUCCESS',
             msg: ''
         });
     }).catch((err) => {
+        console.log(err);
         res.send({
             ack: 'error',
-            msg: ''
+            msg: err.message
+        });
+    });
+});
+
+router.get('/image/list', function(req, res, next) {
+    docker.imagesList().then((data) => {
+        res.send({
+            ack: 'SUCCESS',
+            msg: data
+        });
+    }).catch((err) => {
+        res.send({
+            ack: 'SUCCESS',
+            msg: err.message
+        });
+    });
+});
+
+router.get('/container/status', function(req, res, next) {
+    docker.imagesList().then((data) => {
+        res.send({
+            ack: 'SUCCESS',
+            msg: data
+        });
+    }).catch((err) => {
+        res.send({
+            ack: 'SUCCESS',
+            msg: err.message
         });
     });
 });
 
 router.get('/backup/:projectId', function(req, res, next) {
-	docker.createImage({fromImage: ''}, function (err, stream) {
-		stream.pipe(process.stdout);
-	});
-})
+    const projectId = req.params['projectId'];
+    const name = req.query['name'];
+    const version = req.query['version'];
 
+    docker.commitContainer({
+        projectId: projectId,
+        name: name,
+        version: version
+    }).then((data) => {
+        res.send({
+            ack: 'SUCCESS',
+            msg: data
+        });
+    }).catch((err) => {
+        console.log(err);
+        res.send({
+            ack: 'error',
+            msg: err.message
+        });
+    });
+});
 module.exports = router;
